26a6e313e416178174f9f649c934b48f32f5504f
iskraman <iskraman@gmail.com>
2021-12-09 11:35:50 +0900

    HSet/HGet/HGetAll for redis

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func main() {
 		}
 	*/
 
-	// Redis Cache
+	// Redis Set/Get cache
 	rdb := redislib.New("localhost:6379", "changeme", 0)
 	redislib.Set(rdb, "key1", "my value")
 	val, err := redislib.Get(rdb, "key1") // Exist key
@@ -105,5 +105,21 @@ func main() {
 		syslog.WARLN(err)
 	} else {
 		syslog.STDLN(val2)
+	}
+
+	// Redis HSet/HGet cache
+	server := map[string]interface{}{"cpu": 25.0, "mem": 10.5, "hdd": "40"}
+	redislib.HSet(rdb, "media-server-0", server)
+	redislib.HSet(rdb, "media-server-1", map[string]interface{}{"cpu": 10, "mem": 15, "hdd": "20"})
+
+	cpu, _ := redislib.HGet(rdb, "media-server-0", "cpu")
+	mem, _ := redislib.HGet(rdb, "media-server-0", "mem")
+	hdd, _ := redislib.HGet(rdb, "media-server-0", "hdd")
+	syslog.STD("cpu:%v, mem:%v, hdd:%v", cpu, mem, hdd)
+
+	// Redis HGetAll
+	all, _ := redislib.HGetAll(rdb, "media-server-1")
+	for k, v := range all {
+		syslog.STD("%v(%T):%v(%T)", k, k, v, v)
 	}
 }
--- redislib/redis_cache.go
+++ redislib/redis_cache.go
@@ -13,8 +13,24 @@ func (e errorMsg) Error() string {
 	return e.msg
 }
 
+/*
+https://pkg.go.dev/github.com/go-redis/redis/v8#section-readme
+
+- HSet("myhash", "key1", "value1", "key2", "value2")
+- HSet("myhash", []string{"key1", "value1", "key2", "value2"})
+- HSet("myhash", map[string]interface{}{"key1": "value1", "key2": "value2"})
+*/
+
 func Set(rdb *redis.Client, key string, val string) error {
 	err := rdb.Set(ctx, key, val, 0).Err()
+	if err != nil {
+		syslog.WARLN(err)
+	}
+	return err
+}
+
+func HSet(rdb *redis.Client, key string, val map[string]interface{}) error {
+	err := rdb.HSet(ctx, key, val).Err()
 	if err != nil {
 		syslog.WARLN(err)
 	}
@@ -25,7 +41,29 @@ func Get(rdb *redis.Client, key string) (string, error) {
 	val, err := rdb.Get(ctx, key).Result()
 	if err == redis.Nil {
 		e := errorMsg{msg: "Does not exist key!"}
-		return "", e
+		return val, e
+	} else if err != nil {
+		syslog.WARLN(err)
+	}
+	return val, err
+}
+
+func HGet(rdb *redis.Client, key string, val string) (string, error) {
+	val, err := rdb.HGet(ctx, key, val).Result()
+	if err == redis.Nil {
+		e := errorMsg{msg: "Does not exist key!"}
+		return val, e
+	} else if err != nil {
+		syslog.WARLN(err)
+	}
+	return val, err
+}
+
+func HGetAll(rdb *redis.Client, key string) (map[string]string, error) {
+	val, err := rdb.HGetAll(ctx, key).Result()
+	if err == redis.Nil {
+		e := errorMsg{msg: "Does not exist key!"}
+		return val, e
 	} else if err != nil {
 		syslog.WARLN(err)
 	}
