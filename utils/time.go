b0e7f95fb0c4c0bef46b262afc18e58d2d1cadf0
iskraman <iskraman@gmail.com>
2022-01-18 18:54:12 +0900

    unix time

--- utils/time.go
+++ utils/time.go
@@ -10,3 +10,22 @@ func GetTimeStr() string {
 	date := fmt.Sprintf("%v", timestamp)
 	return date
 }
+
+func GetUnixTimeSec() int64 {
+	now := time.Now()
+	secs := now.Unix()
+	return secs
+}
+
+func GetUnixTimeMil() int64 {
+	now := time.Now()
+	nanos := now.UnixNano()
+	mils := nanos / 1000000
+	return mils
+}
+
+func GetUnixTimeNano() int64 {
+	now := time.Now()
+	nanos := now.UnixNano()
+	return nanos
+}
